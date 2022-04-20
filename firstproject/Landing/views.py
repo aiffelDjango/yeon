a32cf74bd65096e8230fdfb12fae4dc61a527fe5
yeon42 <jungyune22@gmail.com>
2022-04-20 18:59:49 +0900

    prac for portfolio

--- firstproject/Landing/views.py
+++ firstproject/Landing/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request): # 요청을 받으면 index.html을 보여줄 수 있게 이렇게 응답
-    return render(request, 'Landing/index.html')
+    return render(request, 'Landing/index.html') # Landing 폴더에 있는 index.html을 뱉어라.
     # return HttpResponse('Hello')
 
 def study(req):
